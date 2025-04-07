92ad1ac6641398b15ea98aed7a1350cb01d2238a
Jeff Walden <jwalden@mit.edu>
2025-04-07 01:11:55 -0700

    feat: Enable specifying the broadcast for a feedback using a variables-supporting text string, as alternative to selecting from a dropdown list.

--- src/feedbacks.ts
+++ src/feedbacks.ts
@@ -5,14 +5,44 @@ import {
 	CompanionFeedbackAdvancedEvent,
 	DropdownChoice,
 	combineRgb,
+	CompanionMigrationFeedback,
 	CompanionFeedbackContext,
+	SomeCompanionFeedbackInputField,
 } from '@companion-module/base';
 import { Broadcast, BroadcastMap, BroadcastLifecycle, Stream, StreamHealth } from './cache';
+import {
+	broadcastIdDropdownOption,
+	BroadcastIdFromTextOption,
+	BroadcastIdIsTextCheckbox,
+	BroadcastIdIsTextOptionId,
+	BroadcastIdDropdownOptionId,
+	BroadcastIdTextOptionId,
+	getBroadcastIdFromOptions,
+} from './common';
 import { Core } from './core';
 
 export enum FeedbackId {
 	BroadcastStatus = 'broadcast_status',
 	StreamHealth = 'broadcast_bound_stream_health',
+}
+
+export function tryUpgradeFeedbackSelectingBroadcastID(feedback: CompanionMigrationFeedback): boolean {
+	switch (feedback.feedbackId) {
+		case FeedbackId.BroadcastStatus:
+		case FeedbackId.StreamHealth:
+			if (BroadcastIdIsTextOptionId in feedback.options) {
+				return false;
+			}
+			break;
+		default:
+			return false;
+	}
+
+	const options = feedback.options;
+	options[BroadcastIdIsTextOptionId] = false;
+	options[BroadcastIdDropdownOptionId] = options[BroadcastIdTextOptionId] = options.broadcast;
+	delete options.broadcast;
+	return true;
 }
 
 interface AdvancedFeedbackWithAsyncCallback extends CompanionAdvancedFeedbackDefinition {
@@ -58,24 +88,30 @@ export function listFeedbacks(
 
 	const findBroadcastForOptions = async (
 		options: CompanionFeedbackAdvancedEvent['options'],
-		_context: CompanionFeedbackContext
+		context: CompanionFeedbackContext
 	): Promise<Broadcast | undefined> => {
 		if (!checkCore()) {
 			return undefined;
 		}
 
-		const rawBroadcastOption = options.broadcast;
-		if (!rawBroadcastOption) {
+		const id = await getBroadcastIdFromOptions(options, context);
+		if (id === undefined) {
+			core!.Module.log('warn', 'Feedback failed: undefined broadcast ID');
 			return undefined;
 		}
 
-		const id = String(rawBroadcastOption);
 		if (id in core!.Cache.Broadcasts) {
 			return core!.Cache.Broadcasts[id];
 		}
 
 		return core!.Cache.UnfinishedBroadcasts.find((_a, i) => `unfinished_${i}` === id);
 	}
+
+	const selectFromAllBroadcasts: SomeCompanionFeedbackInputField[] = [
+		BroadcastIdIsTextCheckbox,
+		broadcastIdDropdownOption([...broadcastEntries, ...broadcastUnfinishedEntries], defaultBroadcast),
+		BroadcastIdFromTextOption,
+	];
 
 	return {
 		[FeedbackId.BroadcastStatus]: {
@@ -119,13 +155,7 @@ export function listFeedbacks(
 					id: 'text_complete',
 					default: combineRgb(126, 126, 126),
 				},
-				{
-					type: 'dropdown',
-					label: 'Broadcast',
-					id: 'broadcast',
-					choices: [...broadcastEntries, ...broadcastUnfinishedEntries],
-					default: defaultBroadcast,
-				},
+				...selectFromAllBroadcasts,
 			],
 			callback: async (event, context): Promise<CompanionAdvancedFeedbackResult> => {
 				if (!checkCore) return {};
@@ -224,13 +254,7 @@ export function listFeedbacks(
 					id: 'text_no_data',
 					default: combineRgb(255, 255, 255),
 				},
-				{
-					type: 'dropdown',
-					label: 'Broadcast',
-					id: 'broadcast',
-					choices: [...broadcastEntries, ...broadcastUnfinishedEntries],
-					default: defaultBroadcast,
-				},
+				...selectFromAllBroadcasts,
 			],
 			callback: async (event, context): Promise<CompanionAdvancedFeedbackResult> => {
 				if (!checkCore) return {};
--- src/upgrades.ts
+++ src/upgrades.ts
@@ -1,5 +1,6 @@
 import type {
 	CompanionMigrationAction,
+	CompanionMigrationFeedback,
 	CompanionStaticUpgradeProps,
 	CompanionStaticUpgradeResult,
 	CompanionStaticUpgradeScript,
@@ -7,6 +8,7 @@ import type {
 } from '@companion-module/base';
 import { tryUpgradeActionSelectingBroadcastId } from './actions';
 import type { YoutubeConfig } from './config';
+import { tryUpgradeFeedbackSelectingBroadcastID } from './feedbacks';
 
 function ActionUpdater(
 	tryUpdate: (action: CompanionMigrationAction) => boolean
@@ -23,7 +25,23 @@ function ActionUpdater(
 	};
 }
 
+function FeedbackUpdater(
+	tryUpdate: (feedback: CompanionMigrationFeedback) => boolean
+): CompanionStaticUpgradeScript<YoutubeConfig> {
+	return (
+		_context: CompanionUpgradeContext<YoutubeConfig>,
+		props: CompanionStaticUpgradeProps<YoutubeConfig>
+	): CompanionStaticUpgradeResult<YoutubeConfig> => {
+		return {
+			updatedActions: [],
+			updatedConfig: null,
+			updatedFeedbacks: props.feedbacks.filter(tryUpdate),
+		};
+	};
+}
+
 export const UpgradeScripts = [
 	// force separate upgrade scripts onto separate lines
 	ActionUpdater(tryUpgradeActionSelectingBroadcastId),
+	FeedbackUpdater(tryUpgradeFeedbackSelectingBroadcastID),
 ] satisfies CompanionStaticUpgradeScript<YoutubeConfig>[];
